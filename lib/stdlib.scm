c6bba6f359f831b8a6859e0646f12398bf2278a5
Rafael Barreto <rafaelbarreto87@gmail.com>
2011-08-20 20:14:38 -0300

    new stdlib

--- lib/stdlib.scm
+++ lib/stdlib.scm
@@ -1,50 +1,80 @@
-(define (null? lst) (eqv? lst '()))
+;; ====================
+;; High-order utilities
+;; ====================
 
-(define (not x) (if x #f #t))
+(define (compose f g)
+  (lambda x (f (apply g x))))
 
-(define (id x) x)
+;; ==============
+;; List functions
+;; ==============
 
-(define (negate x) (- 0 x))
+(define (null? lst) (equal? lst '()))
 
-(define (list . objs) objs)
+(define (list . lst) lst)
 
-(define (curry func arg1) (lambda (arg) (apply func (cons arg1 (list arg)))))
+(define (caar pair)
+  (car (car pair)))
 
-(define (compose f g) (lambda (arg) (f (apply g arg))))
+(define (cadr pair)
+  (car (cdr pair)))
 
 (define (foldl f ret lst)
-    (if (null? lst)
-        ret
-        (foldl f (f ret (car lst)) (cdr lst))))
+  (if (null? lst)
+      ret
+      (foldl f (f ret (car lst)) (cdr lst))))
 
 (define (foldr f ret lst)
-    (if (null? lst)
-        ret
-        (f (car lst) (foldr f ret (cdr lst)))))
-
-(define (sum . lst) (foldl + 0 lst))
-
-(define (product . lst) (foldl * 1 lst))
-
-(define (and . lst) (foldl && #t lst))
-
-(define (or . lst) (foldl || #f lst))
+  (if (null? lst)
+      ret
+      (f (car lst) (foldr f ret (cdr lst)))))
 
 (define (length lst) (foldl (lambda (x y) (+ x 1)) 0 lst))
 
-(define (map f lst) (foldr (lambda (x y) (cons (f x) y)) '() lst))
+(define (sum lst) (apply + lst))
 
-(define (filter pred lst)
-    (foldr (lambda (x y) (if (pred x) (cons x y) y)) '() lst))
+(define (product lst) (foldl * 1 lst))
 
-(define (filter pred lst)
-    (foldr (lambda (x y) (if (pred x) (cons x y) y)) '() lst))
 
-(define (?) (error "not implemented"))
 
-(begin
-    (display "MyLisp Interpreter")
-    (newline)
-    (newline)
-    (display "Type (?) for a list of commands in standard library or (quit)
-to quit"))
+(define (not b) (if b #f #t))
+
+(define (boolean? obj)
+  (or (equal? obj #t) (equal? obj #f)))
+
+(define (zero? z) (= z 0))
+
+(define (positive? x) (> x 0))
+
+(define (negative? x) (< x 0))
+
+(define (odd? n) (= (remainder n 2) 1))
+
+(define (even? n) (not (odd? n)))
+
+(define (succ n) (+ n 1))
+
+(define (id x) x)
+
+(define (negate x) (- x))
+
+(define (abs x) (if (negative? x) (negate x) x))
+
+(define (range a1 an)
+  (if (> a1 an)
+      '()
+      (cons a1 (range (succ a1) an))))
+
+(define (fact n)
+  (product (range 1 n)))
+
+(define (gcd . lst)
+  (define (iter a b)
+    (if (= b 0)
+        a
+        (iter b (modulo a b))))
+  (abs (cond ((null? lst) 0)
+             ((null? (cdr lst)) (car lst))
+             (else (foldl iter (car lst) (cdr lst))))))
+
+(fact 1000)
